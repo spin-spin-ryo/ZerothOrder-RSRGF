f1c49f55b6c40d3de962428126be93a5bf35a792
spin-spin-ryo <19991217r@gmail.com>
2023-09-11 21:54:54 +0900

    add new proposed method

--- proposed_method.py
+++ proposed_method.py
@@ -64,3 +64,44 @@ class proposed(__optim__):
                 time_values = ("max",time.time() - self.start_time),
                 norm_dir = ("iter",torch.linalg.norm(dk).item()))
         return
+ 
+
+   
+class proposed_heuristic(proposed):
+    def __init__(self, determine_stepsize):
+        super().__init__(determine_stepsize)
+        self.P = None
+    
+    def __iter_per__(self,i):
+        self.__clear__()
+        loss = self.func(self.xk)
+        dk = self.__direction__(loss,iteration = i)
+        lr = self.__step__(i)
+        self.__update__(lr*dk)
+        torch.cuda.synchronize()
+        self.__save_value__(i,fvalues = ("min",loss.item()),
+                time_values = ("max",time.time() - self.start_time),
+                norm_dir = ("iter",torch.linalg.norm(dk).item()))
+        return
+    
+
+    def __direction__(self,loss,**kwargs):
+        with torch.no_grad():
+            reduced_dim = self.params[0]
+            sample_size = self.params[1]
+            mu = self.params[2]
+            interval = self.params[4]
+            dim = self.xk.shape[0]
+            if kwargs["iteration"]%interval == 0:
+                self.P = torch.randn(dim,reduced_dim,device = self.device,dtype = self.dtype)/torch.sqrt(torch.tensor(dim,device = self.device,dtype = self.dtype))
+
+            subspace_func = lambda d: self.func(self.xk + self.P@d)
+            subspace_dir = None
+            U = torch.randn(sample_size,reduced_dim,device = self.device,dtype = self.dtype)/torch.sqrt(torch.tensor(sample_size,device = self.device,dtype = self.dtype))
+            for i in range(sample_size):
+                g1 = subspace_func(mu*U[i])
+                if subspace_dir is None:
+                    subspace_dir = (g1 - loss.item())/mu * U[i]
+                else:
+                    subspace_dir += (g1 - loss.item())/mu * U[i]
+            return - self.P@subspace_dir
