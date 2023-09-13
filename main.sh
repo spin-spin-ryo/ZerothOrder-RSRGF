9058eb2a4dd3d35ac22f849d25752ecb4ae62d6c
spin-spin-ryo <19991217r@gmail.com>
2023-09-13 09:58:27 +0900

    remoce cache

--- main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH -p v
-#SBATCH --gres=gpu:1
-#SBATCH -N 1
-#SBATCH -o /home/u00786/Research/optimization/outputs/output.%j.out
-
-export PATH=/home/app/singularity-ce/bin:$PATH
-singularity exec --nv /home/u00786/Research/Master-Research1/pytorch_22.03-py3.sif python /home/u00786/Research/optimization/runNumerical.py
