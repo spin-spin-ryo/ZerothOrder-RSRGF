74780aee24615306566d1cd8c211d178d282625c
spin-spin-ryo <19991217r@gmail.com>
2024-03-26 21:25:37 +0900

    add make_config.py

---
+++ environments.py
@@ -0,0 +1,12 @@
+import torch
+
+DATAPATH = "./data"
+DTYPE = torch.float64
+
+if torch.cuda.is_available():   
+    DEVICE = "cuda"
+else:
+    DEVICE = "cpu"
+
+CONFIGPATH = "./configs"
+RESULTPATH = "./results"
